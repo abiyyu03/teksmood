6f02710f0543489b5583436860a191c69e369933
abiyyu03 <abiyyu652@gmail.com>
2024-12-08 17:18:22 +0700

    setup react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router'
 import './index.css'
+import Home from './pages/Home'
+import FormPage from './pages/FormPage'
+import EmotionPage from './pages/EmotionPage'
 
 function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/fill" element={<FormPage />} />
+            <Route path="/emotion" element={<EmotionPage />} />
+        </Routes>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				rage: '#A30000',
+				sad: '#5603AD',
+				weary: '#FF6200',
+				happy: '#00A664',
+				accent: '#00A6A6',
+			},
+		},
 	},
 	plugins: [],
 };
