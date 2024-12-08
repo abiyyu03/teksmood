1af1d1769c6e27ac8b9ceb68552c3e42c6630b07
abiyyu03 <abiyyu652@gmail.com>
2024-12-08 17:41:34 +0700

    remove : stricttype

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-    // <StrictMode>
     <BrowserRouter>
         <App />
     </BrowserRouter>
-    // </StrictMode>,
 )
